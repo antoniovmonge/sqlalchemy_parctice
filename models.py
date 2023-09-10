dacad982cbfdcef554fe86898978e4fe7cd3bbe1
Antonio Vilchez <antoniovmonge@gmail.com>
2023-09-10 18:50:32 +0200

    [add] Manufacturer model and relationship

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db import Model
@@ -12,9 +12,22 @@ class Product(Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
     manufacturer: Mapped[str] = mapped_column(String(64), index=True)
+    manufacturer_id: Mapped[int] = mapped_column(
+        ForeignKey("manufacturers.id"), index=True
+    )
     year: Mapped[int] = mapped_column(index=True)
     country: Mapped[Optional[str]] = mapped_column(String(32))
     cpu: Mapped[Optional[str]] = mapped_column(String(32))
 
     def __repr__(self):
         return f"Product({self.id}, '{self.name}')"
+
+
+class Manufacturer(Model):
+    __tablename__ = "manufacturers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return f"Manufacturer({self.id}, '{self.name}')"
