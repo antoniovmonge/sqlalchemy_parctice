1f600955d044a9b1a6588eec383b2ceae1324350
Antonio Vilchez <antoniovmonge@gmail.com>
2023-09-10 19:00:30 +0200

    [add] back_populates field

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db import Model
 
@@ -11,13 +11,15 @@ class Product(Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
-    manufacturer: Mapped[str] = mapped_column(String(64), index=True)
-    manufacturer_id: Mapped[int] = mapped_column(
-        ForeignKey("manufacturers.id"), index=True
-    )
     year: Mapped[int] = mapped_column(index=True)
     country: Mapped[Optional[str]] = mapped_column(String(32))
     cpu: Mapped[Optional[str]] = mapped_column(String(32))
+    # manufacturer_id: Mapped[int] = mapped_column(
+    #     ForeignKey("manufacturers.id"), index=True
+    # )
+    manufacturer: Mapped["Manufacturer"] = relationship(
+        back_populates="products"
+    )
 
     def __repr__(self):
         return f"Product({self.id}, '{self.name}')"
@@ -28,6 +30,9 @@ class Manufacturer(Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    products: Mapped[list["Product"]] = relationship(
+        back_populates="manufacturer"
+    )
 
     def __repr__(self):
         return f"Manufacturer({self.id}, '{self.name}')"
